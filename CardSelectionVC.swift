7ff20e19a100358be0250bdfd3c746d064f5028b
Shubham Lekhak <lekhak.sub@gmail.com>
2023-04-11 01:06:38 +0545

    Add files via upload

---
+++ CardSelectionVC.swift
@@ -0,0 +1,44 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by Shubham Lekhak on 09/02/2023.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    @IBOutlet var cardImageView: UIImageView!
+    var cards: [UIImage] = Card.allValues
+    var timer: Timer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.08, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image =  cards.randomElement() ?? UIImage(named: "AS")
+        
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+    }
+    
+}
